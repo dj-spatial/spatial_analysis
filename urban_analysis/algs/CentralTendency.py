ac8c819db16af00f3b9b21fabd0130dea59c2aeb
dj <dj.paek1@gmail.com>
2024-03-09 22:05:07 +0900

    no message

---
+++ urban_analysis/algs/CentralTendency.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ UrbanReal
+                                 A QGIS plugin
+ Urban Real Estate Analyzer
+                              -------------------
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by D.J Paek
+        email                : dj dot paek1 at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+__author__ = 'D.J Paek'
+__date__ = 'March 2019'
+__copyright__ = '(C) 2019, D.J Paek'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+import os
+
+from qgis.PyQt.QtCore import QVariant
+from qgis.PyQt.QtGui import QIcon
+
+from qgis.core import (QgsProcessingParameterBoolean,
+                       QgsExpression,
+                       QgsFeatureRequest,
+                       QgsField,
+                       QgsFields,
+                       QgsFeatureSink,
+                       QgsProcessing,
+                       QgsProcessingParameterField,
+                       QgsProcessingParameterEnum,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink)
+
+from processing.algs.qgis.QgisAlgorithm import QgisAlgorithm
+from ..utilities import getMeanCenter
+from ..utilities import getMedianCenter
+from ..utilities import getCentralFeature
+from ..utilities import getPointCoords
+
+pluginPath = os.path.split(os.path.split(os.path.dirname(__file__))[0])[0]
+
+
+class CentralTendency(QgisAlgorithm):
+
+    INPUT_POINTS = 'INPUT_POINTS'
+    GROUP_FIELD = 'GROUP_FIELD'
+    WEIGHT_FIELD = 'WEIGHT_FIELD'
+    MEAN_CENTER = 'MEAN_CENTER'
+    MEDIAN_CENTER = 'MEDIAN_CENTER'
+    CENTRAL_FEATURE = 'CENTRAL_FEATURE'
+    METRIC = 'METRIC'
+    OUTPUT_MEAN = 'OUTPUT_MEAN'
+    OUTPUT_MEDIAN = 'OUTPUT_MEDIAN'
+    OUTPUT_CENTRAL = 'OUTPUT_CENTRAL'
+	
+    def icon(self):
+        return QIcon(os.path.join(pluginPath, 'urban_analysis', 'icons', 'central.png'))
+
+    def group(self):
+        return self.tr('Spatial Central Tendency')
+
+    def groupId(self):
+        return 'centraltendency'
+    
+    def name(self):
+        return 'spatialcenters'
+
+    def displayName(self):
+        return self.tr('Centers(평균좌표, 중앙좌표, 중심피쳐)')
+    
+    def msg(self, var):
+        return "Type:"+str(type(var))+" repr: "+var.__str__()
+
+    def __init__(self):
+        super().__init__()
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterFeatureSource(self.INPUT_POINTS,
+                                                              self.tr(u'포인트 레이어'),
+                                                              [QgsProcessing.TypeVectorPoint]))
+        self.addParameter(QgsProcessingParameterField(self.GROUP_FIELD,
+                                                      self.tr('Group Field'),
+                                                      parentLayerParameterName=self.INPUT_POINTS,
+                                                      type=QgsProcessingParameterField.Any, optional=True))
+        self.addParameter(QgsProcessingParameterField(self.WEIGHT_FIELD,
+                                                      self.tr(u'가중치 필드'),
+                                                      parentLayerParameterName=self.INPUT_POINTS,
+                                                      type=QgsProcessingParameterField.Numeric, optional=True))
+        self.addParameter(QgsProcessingParameterBoolean(self.MEAN_CENTER,
+                                                        self.tr(u'평균좌표'),
+                                                        defaultValue = True))
+        self.addParameter(QgsProcessingParameterBoolean(self.MEDIAN_CENTER,
+                                                        self.tr(u'중앙좌표'),
+                                                        defaultValue = True))
+        self.addParameter(QgsProcessingParameterBoolean(self.CENTRAL_FEATURE,
+                                                        self.tr(u'중심피쳐'),
+                                                        defaultValue = True))
+        self.addParameter(QgsProcessingParameterEnum(self.METRIC,
+                                                        self.tr(u'거리단위(중심피쳐에만 적용)'),
+                                                        defaultValue  = 0,
+                                                        options = ['Euclidean', 'City Block']))
+        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_MEAN, 
+                                                            self.tr(u'평균좌표'),
+                                                            QgsProcessing.TypeVectorPoint))
+        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_MEDIAN, 
+                                                            self.tr(u'중앙좌표'),
+                                                            QgsProcessing.TypeVectorPoint))
+        self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT_CENTRAL, 
+                                                            self.tr(u'중심피쳐'),
+                                                            QgsProcessing.TypeVectorPoint))
+
+    def processAlgorithm(self, parameters, context, feedback):
+        feedback.pushInfo(self.tr("Starting Algorithm: '{}'".format(self.displayName())))
+        cLayer = self.parameterAsSource(parameters, self.INPUT_POINTS, context)
+        fields = cLayer.fields()
+        groupField = self.parameterAsString(parameters, self.GROUP_FIELD, context)
+        groupFieldIndex = fields.lookupField(self.parameterAsString(parameters, self.GROUP_FIELD, context))
+        groupList = sorted(cLayer.uniqueValues(groupFieldIndex))
+
+        meanCenter = self.parameterAsBool(parameters, self.MEAN_CENTER, context)
+        medianCenter = self.parameterAsBool(parameters, self.MEDIAN_CENTER, context)
+        centralFeature = self.parameterAsBool(parameters, self.CENTRAL_FEATURE, context)
+        dMetricIndex = self.parameterAsEnum(parameters, self.METRIC, context)
+
+        weightFieldIndex = fields.lookupField(self.parameterAsString(parameters, self.WEIGHT_FIELD, context))
+
+        # create an id field
+        idField = QgsFields()
+        idField.append(QgsField('ID', QVariant.Int))
+        results = {}
+
+        if  meanCenter:
+            (sink_mean, mean_id) = self.parameterAsSink(parameters, self.OUTPUT_MEAN, context,
+                                                        idField, cLayer.wkbType(), cLayer.sourceCrs())
+            if groupFieldIndex > -1 :
+                for group in groupList:
+                    query = '"{field}" = {value}'.format(field = groupField, value = group)
+                    exp = QgsExpression(query)
+                    request = QgsFeatureRequest(exp)
+                    feat = [f for f in cLayer.getFeatures(request)]
+                    pointCoords = getPointCoords(feat, weightFieldIndex)
+                    meanCenterFeat = getMeanCenter(pointCoords[0], pointCoords[1], pointCoords[2], 1)
+                    sink_mean.addFeature(meanCenterFeat, QgsFeatureSink.FastInsert)
+            else:
+                feat = cLayer.getFeatures()
+                pointCoords = getPointCoords(feat, weightFieldIndex)
+                meanCenterFeat = getMeanCenter(pointCoords[0], pointCoords[1], pointCoords[2], 1)
+                sink_mean.addFeature(meanCenterFeat, QgsFeatureSink.FastInsert)
+            results[self.OUTPUT_MEAN] = mean_id
+
+        if  medianCenter:
+            (sink_median, median_id) = self.parameterAsSink(parameters, self.OUTPUT_MEDIAN, context,
+                                                            idField, cLayer.wkbType(), cLayer.sourceCrs())
+            if groupFieldIndex > -1 :
+                for group in groupList:
+                    query = '"{field}" = {value}'.format(field = groupField, value = group)
+                    exp = QgsExpression(query)
+                    request = QgsFeatureRequest(exp)
+                    feat = [f for f in cLayer.getFeatures(request)]
+                    pointCoords = getPointCoords(feat, weightFieldIndex)
+                    medianCenterFeat = getMedianCenter(pointCoords[0], pointCoords[1], pointCoords[2], 1)
+                    sink_median.addFeature(medianCenterFeat, QgsFeatureSink.FastInsert)
+            else:
+                feat = cLayer.getFeatures()
+                pointCoords = getPointCoords(feat, weightFieldIndex)
+                medianCenterFeat = getMedianCenter(pointCoords[0], pointCoords[1], pointCoords[2], 1)
+                sink_median.addFeature(medianCenterFeat, QgsFeatureSink.FastInsert)
+            results[self.OUTPUT_MEDIAN] = median_id
+
+        if  centralFeature:
+            (sink_central, central_id) = self.parameterAsSink(parameters, self.OUTPUT_CENTRAL, context,
+                                                              idField, cLayer.wkbType(), cLayer.sourceCrs())
+            if groupFieldIndex > -1 :
+                for group in groupList:
+                    query = '"{field}" = {value}'.format(field = groupField, value = group)
+                    exp = QgsExpression(query)
+                    request = QgsFeatureRequest(exp)
+                    feat = [f for f in cLayer.getFeatures(request)]
+                    pointCoords = getPointCoords(feat, weightFieldIndex)
+                    centralFeatureFeat = getCentralFeature(pointCoords[0], pointCoords[1], pointCoords[2], 1, dMetricIndex)
+                    sink_central.addFeature(centralFeatureFeat, QgsFeatureSink.FastInsert)
+            else:
+                feat = cLayer.getFeatures()
+                pointCoords = getPointCoords(feat, weightFieldIndex)
+                centralFeatureFeat = getCentralFeature(pointCoords[0], pointCoords[1], pointCoords[2], 1, dMetricIndex)
+                sink_central.addFeature(centralFeatureFeat, QgsFeatureSink.FastInsert)
+            results[self.OUTPUT_CENTRAL] = central_id
+        return results
---
+++ urban_analysis/forms/DbscanKnn.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+                                 A QGIS plugin
+ Urban Analyzer
+                              -------------------
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by D.J Paek
+        email                : dj dot paek1 at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+__author__ = 'D.J Paek'
+__date__ = 'March 2019'
+__copyright__ = '(C) 2019, D.J Paek'
+
+# This will get replaced with a git SHA1 when you do a git archive
+
+__revision__ = '$Format:%H$'
+
+from processing.gui.wrappers import WidgetWrapper, DIALOG_STANDARD
+from processing.tools import dataobjects
+import os
+import tempfile
+import numpy as np
+from scipy.spatial import KDTree
+import plotly
+import plotly as plt
+import plotly.graph_objs as go
+from qgis.PyQt import uic
+from qgis.core import QgsNetworkAccessManager, QgsProject 
+from qgis.PyQt.QtWidgets import QVBoxLayout
+from qgis.PyQt.QtWebKit import QWebSettings
+from qgis.PyQt.QtWebKitWidgets import QWebView
+from qgis.PyQt.QtCore import QUrl
+from PyQt5.QtWidgets import QMessageBox
+
+pluginPath = os.path.dirname(__file__)
+WIDGET, BASE = uic.loadUiType(
+    os.path.join(pluginPath, 'DbscanKnn.ui'))
+
+class KnnWidget(BASE, WIDGET):
+
+    def __init__(self):
+        super(KnnWidget, self).__init__(None)
+        self.setupUi(self)
+
+        # load the webview of the plot
+        self.knn_webview_layout = QVBoxLayout()
+        self.knn_webview_layout.setContentsMargins(0,0,0,0)
+        self.knn_panel.setLayout(self.knn_webview_layout)
+        self.knn_webview = QWebView()
+        self.knn_webview.page().setNetworkAccessManager(QgsNetworkAccessManager.instance())
+        knn_webview_settings = self.knn_webview.settings()
+        knn_webview_settings.setAttribute(QWebSettings.WebGLEnabled, True)
+        knn_webview_settings.setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
+        knn_webview_settings.setAttribute(QWebSettings.Accelerated2dCanvasEnabled, True)
+        self.knn_webview_layout.addWidget(self.knn_webview)
+
+        # Connect signals
+        self.knnBtn.clicked.connect(self.plotView)
+        self.browserBtn.clicked.connect(self.browserVeiw)
+
+    def getKnn(self):
+        cLayer =  QgsProject.instance().mapLayer(self.vid)
+        if cLayer is None:
+            msg = u'레이어가 선택되지 않았습니다.'
+            return msg
+
+        # get coordinates of point features
+        pts=[f.geometry().asPoint() for f in cLayer.getFeatures()]            
+        x=[pt[0] for pt in pts]
+        y=[pt[1] for pt in pts]
+        coords = np.stack([x, y], axis = -1)
+        if self.K > len(coords):
+            msg = u'데이터의 숫자보다 군집수가 더 많습니다.'
+            return msg
+
+        ## get distance tree
+        tree = KDTree(coords)
+
+        ## distance and index of k nearest neighbors from each point
+        dist, idx = tree.query(coords, self.K+1)
+        knndist = dist[:, self.K]
+
+        knndist = np.sort(knndist)
+
+        #그래프
+        x_range = np.arange(1, len(knndist) + 1)
+        knnHover = ['{0:,.3f}'.format(k) for k in list(knndist)]
+        trace0 = go.Scatter(
+            x=x_range,
+            y=knndist,
+            text=knnHover,
+            mode="lines",
+            line=dict(color='orange'),
+            hoverinfo='text',
+            hoverlabel=dict(bgcolor='white'),
+            textposition='top center',
+            name=u'최근린 ' + str(self.K) + u'번째까지 거리'
+        )    
+        x_axis = {
+            'title': u'최근린 K번째까지 거리',
+            'side': 'bottom',
+            'zeroline': False,
+            'showline': True,
+            'showgrid': False,
+            'autorange': True,
+            'rangemode': 'nonnegative',
+            'visible': True
+        }
+        y_axis = {
+            'type': '-',
+            'ticks': '',
+            'zeroline': False,
+            'showline': True,
+            'showgrid': False,
+            'autorange': True,
+            'rangemode': 'nonnegative',
+            'visible': True,
+            'mirror':False
+        }
+        layout = {
+            'plot_bgcolor': 'white',
+            'hovermode': 'closest',
+            'hoverlabel': dict(font=dict(color='black')),
+            'legend': dict(x=0.80, y=1, borderwidth = 0, orientation='v', traceorder='normal', tracegroupgap = 5, font={'size':12}),
+            'showlegend': True,
+            'xaxis1': dict(x_axis, **dict(domain=[0.0, 1.0], anchor='y1')),
+            'yaxis1': dict(y_axis, **dict(domain=[0.0, 1.0], anchor='x1')),
+            'margin': dict(l=30, r=15, t=15, b=45)
+        }
+        data = [trace0]
+        self.fig = {'data' : data, 'layout' : layout}
+        msg = 'Success'
+        return msg
+        
+    def plotView(self):
+        msg = self.getKnn()
+        if  msg != 'Success':
+            QMessageBox.information(self, u"입력 값 오류", msg)
+        else:
+            config = {'scrollZoom': True, 'editable': False, 'displayModeBar': False}
+            raw_plot = plotly.offline.plot(self.fig, output_type='div', config=config, show_link = False)
+            plot_path = os.path.join(tempfile.gettempdir(), 'knn'+'.html')
+            with open(plot_path, "w") as f:
+                f.write(raw_plot)
+            widget_layout = self.knn_webview_layout
+            webview =  self.knn_webview
+            plot_url = QUrl.fromLocalFile(plot_path)
+            webview.load(plot_url)
+            widget_layout.addWidget(webview)
+
+    def browserVeiw(self):
+        msg = self.getKnn()
+        if  msg != 'Success':
+            QMessageBox.information(self, u"입력 값 오류", msg)
+        else:
+            plt.offline.plot(self.fig, filename = os.path.join(tempfile.gettempdir(), 'knn'+'.html') , auto_open=True)
+        
+    def setLayer(self, layer):
+        self.vid = layer
+        
+    def setK(self, k):
+        self.K = k
+
+    def value(self):
+        return [1]
+
+class KnnWidgetWrapper(WidgetWrapper):
+
+    def __init__(self, param, dialog, row=3, col=0, **kwargs):
+        super().__init__(param, dialog, row, col, **kwargs)
+        self.context = dataobjects.createContext()
+
+    def _panel(self):
+        return KnnWidget()
+
+    def createWidget(self):
+        if self.dialogType == DIALOG_STANDARD:
+            return self._panel()
+
+    def postInitialize(self, wrappers):
+        if self.dialogType != DIALOG_STANDARD:
+            return
+        for wrapper in wrappers:
+            if wrapper.parameterDefinition().name() == self.param.layer_param:
+                self.setLayer(wrapper.parameterValue())
+                wrapper.widgetValueHasChanged.connect(self.layerChanged)
+            elif wrapper.parameterDefinition().name() == self.param.k_param:
+                self.setK(wrapper.parameterValue())
+                wrapper.widgetValueHasChanged.connect(self.kChanged)
+
+    def layerChanged(self, wrapper):
+        self.setLayer(wrapper.parameterValue())
+
+    def setLayer(self, layer):
+        self.widget.setLayer(layer)
+
+    def kChanged(self, wrapper):
+        self.setK(wrapper.parameterValue())
+
+    def setK(self, k):
+        self.widget.setK(k)
+
+    def value(self):
+        return self.widget.value()
+
